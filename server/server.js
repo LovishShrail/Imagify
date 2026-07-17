accbc537ed49720f665415557cd4e9d0052fed1e
Lovish1201 <lovishshrail10@gmail.com>
2026-07-18 02:09:14 +0530

    fixed deployment issues

--- server/server.js
+++ server/server.js
@@ -12,7 +12,10 @@ await connectDB()
 
 // Intialize Middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+  credentials: true
+}))
 
 // API routes
 app.use('/api/user',userRouter)
